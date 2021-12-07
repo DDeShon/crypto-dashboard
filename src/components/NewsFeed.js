51d8bbfcdc3281188ee15c423a00ea5b20b1ea17
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-12-06 20:13:13 -0600

    setting up crypto news feed

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const NewsFeed = () => {
-  return <div className="news-feed">NewsFeed</div>;
+  const [articles, setArticles] = useState(null);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://crypto-news-live3.p.rapidapi.com/news",
+      headers: {
+        "x-rapidapi-host": "crypto-news-live3.p.rapidapi.com",
+        "x-rapidapi-key": "77781d9952msh652064533e934a7p1c14d8jsn17b5c44455c4",
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+        setArticles(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(articles);
+
+  const first14Articles = articles?.slice(0, 14);
+
+  return (
+    <div className="news-feed">
+      <h1>NewsFeed</h1>
+      {first14Articles?.map((article) => (
+        <p>{article.title}</p>
+      ))}
+    </div>
+  );
 };
 
 export default NewsFeed;
