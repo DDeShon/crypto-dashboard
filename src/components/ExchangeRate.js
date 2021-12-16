0e82d80a31fc3394361f42f91f3482a8097b6b08
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-12-16 09:47:23 -0600

    working on styling the page

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -2,7 +2,7 @@ const ExchangeRate = ({ exchangedData }) => {
   return (
     <div className="exchange-rate">
       <h3>Exchange Rate:</h3>
-      <h1>{exchangedData.exchangeRate}</h1>
+      <h2>{exchangedData.exchangeRate}</h2>
       <p>
         {exchangedData.primaryCurrency} to {exchangedData.secondaryCurrency}
       </p>
